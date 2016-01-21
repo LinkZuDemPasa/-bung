c7992a393790bd1f3253473801c5219629e62450
ThomasVie <thomas@careerharbour.co.uk>
2016-01-21 10:49:33 +0100

    hellow world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
 public static void main(String []args){
 	System.out.println("Hello zu dem World");
+	System.out.println("Hellow World from se other side! xoxo Thomas");
 }
 }
