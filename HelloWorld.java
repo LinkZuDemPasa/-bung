1e756272cbf355851de297002e0c901347805b6e
CubeHunt3R <apurger@student.tgm.ac.at>
2016-01-21 11:10:13 +0100

    BOIS

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 public static void main(String []args){
 	System.out.println("Hello zu dem World");
 	System.out.println("Hellow World from se other side! xoxo Thomas");
+	System.out.println("Hello World IHR SICKEN BOIS JO");
 }
 }
